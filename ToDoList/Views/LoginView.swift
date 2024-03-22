f9dd392fbadb2200e66fb5fda103516679673423
edgarnicolas <guzman.edgar@aol.com>
2024-03-22 17:46:58 -0400

    Creation of login screen design.

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        VStack{
-            //Header
-            HeaderView()
+        NavigationView{
+            VStack{
+                //Header
+                HeaderView()
+                
+                //Login Form
+                Form {
+                    TextField("Email", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        //Attemp Log in
+                        
+                    }label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                            
+                        }
+                        .padding(.top, 30)
+                    }
+                }
+                .scrollContentBackground(.hidden)
+                
+                //Create Account
+                VStack{
+                    Text("New here?")
+                    NavigationLink("Create an account",
+                                   destination: RegisterView())
+                }
+                .padding(.top, 10)
+                
+                Spacer()
+            }
             
-            //Login Form
-            
-            Spacer()
         }
     }
 }
