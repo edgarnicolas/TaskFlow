0432947475528e85cd700843bc3a61d880569f96
edgarnicolas <guzman.edgar@aol.com>
2024-03-23 21:13:17 -0400

    Create a login form

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,48 +8,19 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State var email = ""
-    @State var password = ""
     
     var body: some View {
         NavigationView{
             VStack{
+                
                 //Header
                 HeaderView()
                 
                 //Login Form
-                Form {
-                    TextField("Email", text: $email)
-                        .textFieldStyle(DefaultTextFieldStyle())
-                    
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(DefaultTextFieldStyle())
-                    
-                    Button {
-                        //Attemp Log in
-                        
-                    }label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color.blue)
-                            
-                            Text("Log In")
-                                .foregroundColor(Color.white)
-                                .bold()
-                            
-                        }
-                        .padding(.top, 30)
-                    }
-                }
-                .scrollContentBackground(.hidden)
+                LoginBodyView()
                 
                 //Create Account
-                VStack{
-                    Text("New here?")
-                    NavigationLink("Create an account",
-                                   destination: RegisterView())
-                }
-                .padding(.top, 10)
+                NewHereView()
                 
                 Spacer()
             }
